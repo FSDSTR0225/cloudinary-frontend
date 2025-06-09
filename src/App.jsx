df7950f83e9c7b9f42b4f362fcc48303ece65a03
Jordi Galobart Salvat <jgalobart@gmail.com>
2025-06-09 11:54:33 +0200

    move to react form

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,31 @@ import { useState } from 'react'
 import './cloudinary.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    
+    try {
+      const response = await fetch('http://localhost:3003/profile/edit', {
+        method: 'POST',
+        body: formData
+      })
+      const result = await response.json()
+      console.log(result)
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  }
 
   return (
     <>
-      <form action="http://localhost:3003/profile/edit" method="POST">
+      <form onSubmit={handleSubmit}>
         <h1>Form cloudinary</h1>
-        <label htmlFor="">Nombre del archivo</label>
-        <input type="text" />
-        <label htmlFor="">Archivo</label>
-        <input type="file" />
+        <label htmlFor="filenane">Nombre del archivo</label>
+        <input type="text" name="filename"/>
+        <label htmlFor="file">Archivo</label>
+        <input type="file" name="file" />
         <button type="submit">Enviar</button>
       </form>
     </>
